0cb025344181b2b32da70333aeceb0d1f36c2b2b
Maycon Marques <mayconmarquesh@gmail.com>
2025-10-07 18:19:26 -0300

    fix: antd theme provider

---
+++ src/providers/antd-config-provider.tsx
@@ -0,0 +1,19 @@
+import { ConfigProvider, theme } from "antd"
+import { useTheme } from "../contexts/theme-context"
+
+export function AntdConfigProvider({
+	children,
+}: {
+	children: React.ReactNode
+}) {
+	const { isDarkMode } = useTheme()
+	return (
+		<ConfigProvider
+			theme={{
+				algorithm: isDarkMode ? theme.darkAlgorithm : theme.defaultAlgorithm,
+			}}
+		>
+			{children}
+		</ConfigProvider>
+	)
+}
--- src/providers/app-provider.tsx
+++ src/providers/app-provider.tsx
@@ -1,9 +1,8 @@
 import { QueryClientProvider } from "@tanstack/react-query"
-import { ConfigProvider, theme } from "antd"
 import { BrowserRouter } from "react-router"
 
-import { useTheme } from "../hooks/theme"
 import { queryClient } from "../libs/tanstack-query"
+import { AntdConfigProvider } from "./antd-config-provider"
 import { ThemeProvider } from "./theme-provider"
 
 export default function AppProvider({
@@ -11,20 +10,11 @@ export default function AppProvider({
 }: {
 	children: React.ReactNode
 }) {
-	const { isDarkMode } = useTheme()
 	return (
 		<BrowserRouter>
 			<QueryClientProvider client={queryClient}>
 				<ThemeProvider>
-					<ConfigProvider
-						theme={{
-							algorithm: isDarkMode
-								? theme.darkAlgorithm
-								: theme.defaultAlgorithm,
-						}}
-					>
-						{children}
-					</ConfigProvider>
+					<AntdConfigProvider>{children}</AntdConfigProvider>
 				</ThemeProvider>
 			</QueryClientProvider>
 		</BrowserRouter>
